48d348df174a21ff4601bffa0b8de2f7e3b0a011
Sergio Enrique Vargas <sergioenrique@me.com>
2015-07-29 02:59:08 -0500

    Pruebas para poder ver WDSL de oracle

--- src/controllers.php
+++ src/controllers.php
@@ -6,6 +6,29 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+use Zend\Soap\Client;
+
+class StupidWrapperForOracleServer extends SoapClient {
+    protected function callCurl($url, $data, $action) {
+        $handle   = curl_init();
+        curl_setopt($handle, CURLOPT_URL, $url);
+        curl_setopt($handle, CURLOPT_HTTPHEADER, Array("Content-Type: text/xml", 'SOAPAction: "' . $action . '"'));
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($handle, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($handle, CURLOPT_SSLVERSION, 2);
+        $response = curl_exec($handle);
+        if (empty($response)) {
+            throw new SoapFault('CURL error: '.curl_error($handle),curl_errno($handle));
+        }
+        curl_close($handle);
+        return $response;
+    }
+
+    public function __doRequest($request,$location,$action,$version,$one_way = 0) {
+        return $this->callCurl($location, $request, $action);
+    }
+}
+
 $app->before(function () use ($app) {
     $app['base_url'] = $app['request_stack']->getCurrentRequest()->getScheme().'://'.$app['request_stack']->getCurrentRequest()->getHttpHost().$app['request_stack']->getCurrentRequest()->getBaseUrl();
     ini_set("soap.wsdl_cache_enabled", "0");
@@ -13,11 +36,32 @@ $app->before(function () use ($app) {
 });
 
 $app->get('/client', function () use ($app) {
-    $oracleWebServiceWSDL = "https://caxj.crm.us2.oraclecloud.com/crmCommonSalesParties/SalesPartyService?WSDL";
-    //$oracleWebServiceWSDL = "http://www.webservicex.com/globalweather.asmx?WSDL";
 
-    $client = new Zend\Soap\Client($oracleWebServiceWSDL);
-    $personParty = $client->createPersonPartyAsyncResponse();
+    $oracleWebServiceWSDL = "https://caxj.crm.us2.oraclecloud.com/crmCommonSalesParties/SalesPartyService?WSDL";
+    //$oracleWebServiceWSDL = "https://webtjener09.kred.no/TestWebservice/OppdragServiceSoapHttpPort?WSDL";
+
+    file_put_contents(dirname(__FILE__) .'/Server.wsdl', get_wsdl()); //get_wsdl uses the same wrapper as above
+    $client = new StupidWrapperForOracleServer(dirname(__FILE__) .'/Server.wsdl',array('trace'=>1,'cache_wsdl'=>0));
+
+    //$client = new StupidWrapperForOracleServer($oracleWebServiceWSDL);
+    $personParty = $client->getSalesAccount();
+
+    return new Response($app['serializer']->serialize($personParty), 200, array(
+        "Content-Type" => "application/json"
+    ));
+
+    /*
+
+    $options = array(
+                //'trace' => 1,
+                //'location' => 'https://caxj.crm.us2.oraclecloud.com/crmCommonSalesParties/SalesPartyService',
+            );
+
+    $client = new StupidWrapperForOracleServer($oracleWebServiceWSDL, $options);
+    $client->setHttpLogin('prueba');
+    $client->setHttpPassword('aN1m4La');
+
+    $personParty = $client->getSalesAccount();
 
     return new Response($app['serializer']->serialize($personParty), 200, array(
         "Content-Type" => "application/json"
