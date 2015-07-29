aeca6e71276b4ad41314604837e27840a7d4b371
Sergio Enrique Vargas <sergioenrique@me.com>
2015-07-29 17:28:35 -0500

    Revert "copiando en local"
    This reverts commit 7f8f2f1fb8266c6198bbcf5b529b165e65239c3c.
    
    Conflicts:
    	src/controllers.php

--- src/controllers.php
+++ src/controllers.php
@@ -8,27 +8,6 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 use Zend\Soap\Client;
 
-class StupidWrapperForOracleServer extends SoapClient {
-    protected function callCurl($url, $data, $action) {
-        $handle   = curl_init();
-        curl_setopt($handle, CURLOPT_URL, $url);
-        curl_setopt($handle, CURLOPT_HTTPHEADER, Array("Content-Type: text/xml", 'SOAPAction: "' . $action . '"'));
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($handle, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($handle, CURLOPT_SSLVERSION, 2);
-        $response = curl_exec($handle);
-        if (empty($response)) {
-            throw new SoapFault('CURL error: '.curl_error($handle),curl_errno($handle));
-        }
-        curl_close($handle);
-        return $response;
-    }
-
-    public function __doRequest($request,$location,$action,$version,$one_way = 0) {
-        return $this->callCurl($location, $request, $action);
-    }
-}
-
 $app->before(function () use ($app) {
     $app['base_url'] = $app['request_stack']->getCurrentRequest()->getScheme().'://'.$app['request_stack']->getCurrentRequest()->getHttpHost().$app['request_stack']->getCurrentRequest()->getBaseUrl();
     ini_set("soap.wsdl_cache_enabled", "0");
@@ -37,43 +16,26 @@ $app->before(function () use ($app) {
 
 $app->get('/client', function () use ($app) {
 
-    $oracleWebServiceWSDL = "https://caxj.crm.us2.oraclecloud.com/crmCommonSalesParties/SalesPartyService?WSDL";
-    //$oracleWebServiceWSDL = "https://webtjener09.kred.no/TestWebservice/OppdragServiceSoapHttpPort?WSDL";
+    $url = "https://caxj.crm.us2.oraclecloud.com/crmCommonSalesParties/SalesPartyService?WSDL";
 
-//    file_put_contents(dirname(__FILE__) .'/Server.wsdl', get_wsdl()); //get_wsdl uses the same wrapper as above
-//    $client = new StupidWrapperForOracleServer(dirname(__FILE__) .'/Server.wsdl',array('trace'=>1,'cache_wsdl'=>0));
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_REFERER, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $result = curl_exec($ch);
 
-    $action = '';
-    $data = '';
+    if (empty($result)) {
+        return new Response(new SoapFault('CURL error: '.curl_error($ch),curl_errno($ch)));
+    }
+    curl_close($ch);
 
+    file_put_contents(dirname(__FILE__).'/../web/Server.wsdl', $result);
 
-    $handle = curl_init();
-        curl_setopt($handle, CURLOPT_URL, $oracleWebServiceWSDL);
-        curl_setopt($handle, CURLOPT_HTTPHEADER, Array("Content-Type: text/xml", 'SOAPAction: "' . $action . '"'));
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($handle, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($handle, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-        $response = curl_exec($handle);
-        if (empty($response)) {
-            return new Response(new SoapFault('CURL error: '.curl_error($handle),curl_errno($handle)));
-        }
-        curl_close($handle);
+    $client = new SoapClient(dirname(__FILE__).'/../web/Server.wsdl');
 
-    return new Response($response);
-
-
-
-
-
-    $options = array(
-        'trace' => 1,
-        'use' => SOAP_LITERAL,
-        'style' => SOAP_DOCUMENT,
-    );
-
-    $client = new SoapClient($oracleWebServiceWSDL, $options);
-
-    $params = new \SoapVar("<Echo><Acquirer><Id>MyId</Id><UserId>MyUserId</UserId><Password>MyPassword</Password></Acquirer></Echo>", XSD_ANYXML);
     $result = $client->MethodNameIsIgnored($params);
 
     return new Response($result, 200, array(
