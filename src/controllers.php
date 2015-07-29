c73d928aec9ffdd99e93f6f335e9d6936afdaa39
Sergio Enrique Vargas <sergioenrique@me.com>
2015-07-29 00:08:58 -0500

    Pruebas con serializacion de resultado de wsdl

--- src/controllers.php
+++ src/controllers.php
@@ -22,7 +22,8 @@ class SoapService
 
 $app->before(function () use ($app) {
     $app['base_url'] = $app['request_stack']->getCurrentRequest()->getScheme().'://'.$app['request_stack']->getCurrentRequest()->getHttpHost().$app['request_stack']->getCurrentRequest()->getBaseUrl();
-    ini_set("soap.wsdl_cache_enabled", "0");
+    //ini_set("soap.wsdl_cache_enabled", "0");
+    $app['serializer'] = Zend\Serializer\Serializer::factory('phpserialize');
 });
 
 $app->match('/', function () use ($app) {
@@ -43,8 +44,22 @@ $app->get('/wsdl', function () use ($app) {
 
 $app->get('/client', function () use ($app) {
     //$oracleWebServiceWSDL = "https://caxj.crm.us2.oraclecloud.com/crmCommonSalesParties/SalesPartyService?WSDL";
-    $client = new Zend\Soap\Client($app['base_url'].'/wsdl');
-    return $client->hello("igor");
+    $oracleWebServiceWSDL = "http://www.webservicex.com/globalweather.asmx?WSDL";
+
+    $options = array('CountryName' => 'Mexico', 'CityName' => 'Puebla');
+
+    $client = new Zend\Soap\Client($oracleWebServiceWSDL);
+    $clima = $client->GetWeather($options);
+
+    //return new Response(get_class_vars($clima));
+    return new Response($clima->GetWeatherResult, 200, array(
+        "Content-Type" => "application/xml"
+    ));
+    return new Response($app['serializer']->serialize($clima->GetWeatherResult), 200, array(
+        "Content-Type" => "application/json"
+    ));
+
+    //return $client->createPersonPartyAsyncResponse();
 });
 
 $app->error(function (\Exception $e, Request $request, $code) use ($app) {
